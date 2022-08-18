198483cbf15df1e239a8710b84de909675b0bc47
Alexandre Beiruth <101944430+Alebeiruth@users.noreply.github.com>
2022-08-18 17:19:37 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+  
+ int appleQuantity;
+ double applePrice = 1.49;
+ double appleReview = 823 / 9;
+ int appleReviewDisplay;
+ const char appleLocation = 'F';
+ int dayOfWeek = 0;
+ dayOfWeek += 5;
+appleReview = (823 + 52)/10;
+
+ appleQuantity = 23;
+ appleReview = 82.5;
+ appleReviewDisplay = appleReview;
+
+ if(dayOfWeek % 7 == 3 || appleQuantity < 10)
+ {
+  printf("Sale on apples today, today only they are: $%.2f\n", applePrice * .9);
+ }
+  
+printf("An apple costs: $%.2f, there are %d in inventory found in section: %c and your customers gave it an average review of %d%%!", applePrice, appleQuantity, appleLocation, appleReviewDisplay);
+
+}
